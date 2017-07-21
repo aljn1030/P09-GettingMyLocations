0b201b28814ad6fd5e92a82c09c6abe58d0e53ca
15017185 <15017185@myrp.edu.sg>
2017-07-21 15:30:59 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/p09_gettingmylocations/MyService.java
@@ -0,0 +1,29 @@
+package sg.edu.rp.c346.p09_gettingmylocations;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MyService extends Service {
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return Service.START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
